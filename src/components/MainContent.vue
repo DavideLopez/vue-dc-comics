96680db7c67b7ba22e19d02c26927e3005e609f8
DavideLopez <davidelopez7@gmail.com>
2022-09-06 18:41:45 +0200

    fix errore battitura css per cards

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,98 +1,95 @@
 <template>
     <main>
-     <div class="jumbo">
-        <div class="container">
-            <h1 class="jumbo-title">CURRENT SERIES</h1>
+        <div class="hero">
+            <div class="container">
+                <h2 class="hero-title">CURRENT SERIES</h2>
+            </div>
         </div>
-
-     </div>
-     <div class="series">
-        <div class="container list">
-             <ProductCard class="fumetti" v-for="(serie,index) in series" :key="index" :src="serie.thumb" :title="serie.series"/>
+        <div class="series">
+            <div class="container serie-list">
+                <ProductCard class="serie-item" v-for="(serie, index) in series" :key="index" :src="serie.thumb" :title="serie.series" />
+            </div>
+            <div class="container cta">
+                <a href="#">LOAD MORE</a>
+            </div>
         </div>
-        <div class="container cta">
-            <a href="#"> LOAD MORE </a>
-        </div>
-     </div>
-        
     </main>
-    
 </template>
 
-
 <script>
-import ProductCard from './ProductCard.vue';
-import series from '../dc-comics.js'
+    import ProductCard from './ProductCard.vue';
+    import series from '../dc-comics.js';
 
     export default {
-    components: { 
-        ProductCard,
-    },
-    data() {
-        return {
-            series: series
+        components: {
+            ProductCard
+        },
+        data() {
+            return {
+                series: series
+            }
         }
     }
-}
 </script>
 
 <style lang="scss" scoped>
     main {
+        .hero {
+            background: url('../assets/jumbotron.jpg') top/cover no-repeat;
+            background-color: #1c1c1c;
 
-       .jumbo {
-         height: 400px;
-         background: url('../assets/jumbotron.jpg');
-
-         .container {
+            .container {
                 height: 400px;
                 position: relative;
             }
 
-            .jumbo-title {
+            .hero-title {
                 padding: 0.5rem 1rem;
                 position: absolute;
                 bottom: 0;
                 left: 1rem;
                 transform: translateY(+50%);
                 background-color: #0282f9;
-                color: white;
-                
+                color: #fff;
             }
-       }
-          .series {
-              padding: 3rem;
-              color: white;
-              background-color: black;
-          }
+        }
 
-          .list {
-            display: flex;
-            flex-wrap: nowrap; 
-            margin-bottom: 50px;
-          }
+        .series {
+            padding: 3rem 0;
+            color: white;
+            background-color: #1c1c1c;
 
-          .fumetti {
-            flex-basis: 0;
-            flex-grow: 1;
-            padding: 0 0 10px;
-          }
+            .serie-list {
+                margin-bottom: 1.5rem;
+                display: flex;
+                flex-wrap: wrap;
+                gap: 1.5rem;
 
-          .cta {
-            text-align: center;
-
-            a {
-                padding: 12px 30px;
-                line-height: 25px;
-                background-color: #0282f9;
-                font-weight: bold;
+                .serie-item {
+                    flex-basis: 185px;
+                    flex-grow: 1;
+                    padding: 0 0.75rem;
+                }
             }
 
-            a:hover {
-                opacity: 0.5;
+
+            .cta {
+                text-align: center;
+
+                a {
+                    display: inline-block;
+                    padding: 10px 30px;
+                    line-height: 25px;
+                    background-color: #0282f9;
+                    font-weight: bold;
+                    
+                    &:hover {
+                        opacity: 0.5;
+                    }
+                }
+
+
             }
-          }
+        }
     }
-
-    
-
 </style>
--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,23 +1,29 @@
 <template>
     <div class="card">
         <img :src="src" :alt="title">
-        <h1> {{ title }} </h1>
+        <h3>{{ title }}</h3>
     </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            src: String,
-            title: String
-        }
+export default {
+    props: {
+        src: String,
+        title: String
     }
+}
 </script>
 
 <style lang="scss" scoped>
-     img {
+    img {
+        width: 100%;
         aspect-ratio: 1 / 1;
         object-fit: cover;
         object-position: top;
+    }
+
+    h3 {
+        font-size: 14px;
+        text-transform: uppercase;
     }
 </style>
